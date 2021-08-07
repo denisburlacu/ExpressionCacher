c83214a4e872f134a076d0c0c3214fea74075663
Denis Burlacu <denis.burlacu@ati.utm.md>
2021-08-07 17:53:49 +0300

    1 class 1 file in benchmark project
    Finally doing some cleaning

---
+++ ExpressionCacher.Benchmark/ExpressionBenchmark.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq.Expressions;
+using BenchmarkDotNet.Attributes;
+
+namespace ExpressionCacher.Benchmark
+{
+    public class ExpressionBenchmark
+    {
+        private readonly Product _product;
+
+        public ExpressionBenchmark()
+        {
+            _product = new Product()
+                      {
+                          Id = 1,
+                          Name = "Test1"
+                      };
+        }
+
+        [Benchmark]
+        public string RegularProperty()
+        {
+            return _product.Name;
+        }
+
+        [Benchmark]
+        public string Compile()
+        {
+            Expression<Func<Product, string>> expression = product1 => product1.Name;
+
+            return expression.Compile()(_product);
+        }
+        
+        [Benchmark]
+        public string ExpressionCacher()
+        {
+            Expression<Func<Product, string>> expression = product1 => product1.Name;
+
+            return expression.ToFunc()(_product);
+        }
+    }
+}
---
+++ ExpressionCacher.Benchmark/Product.cs
@@ -0,0 +1,8 @@
+namespace ExpressionCacher.Benchmark
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
